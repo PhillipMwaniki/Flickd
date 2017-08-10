4e6b38ea9b51bc6a8046889fc9f770aec0d1694b
Phillip Nzuli <mwanikiphillip@gmail.com>
2017-08-10 14:34:22 +0300

    Sic parvis magna

---
+++ app/src/main/java/ke/co/philsoft/flickd/GetRawData.java
@@ -0,0 +1,19 @@
+package ke.co.philsoft.flickd;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by mwani on 10-Aug-17.
+ */
+
+class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG = "GetRawData";
+
+    private DownloadStatus mDownloadStatus;
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
