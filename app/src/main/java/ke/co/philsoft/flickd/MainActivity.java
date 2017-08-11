0232eac696d055682605ba94a58726be7c256912
Phillip Nzuli <mwanikiphillip@gmail.com>
2017-08-11 19:50:59 +0300

    Added item click listeners

--- app/src/main/java/ke/co/philsoft/flickd/MainActivity.java
+++ app/src/main/java/ke/co/philsoft/flickd/MainActivity.java
@@ -8,11 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable{
+public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable,
+                                RecyclerItemClickListener.OnRecyclerClickListener{
     private static final String TAG = "MainActivity";
     private FlickrRecyclerViewAdapter mFlickrRecyclerViewAdapter;
 
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mFlickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(this, new ArrayList<Photo>());
         recyclerView.setAdapter(mFlickrRecyclerViewAdapter);
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView, this));
 
     }
 
@@ -70,5 +74,17 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
             Log.e(TAG, "onDataAvailable: onDownloadComplete failed with status " + status );
         }
         Log.d(TAG, "onDataAvailable: ends");
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.d(TAG, "onItemClick: starts");
+        Toast.makeText(MainActivity.this, "Normal tap at position " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemLongClick(View view, int position) {
+        Log.d(TAG, "onItemLongClick: starts");
+        Toast.makeText(MainActivity.this, "Long tap at position " + position, Toast.LENGTH_SHORT).show();
     }
 }
